3e07c9d3e4cf8ffa345c8e57c8968bad0ac28a3a
Md Shahdat Hossain <166033634+MShahdat@users.noreply.github.com>
2025-07-15 23:15:29 +0600

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div id='footer' className='flex justify-around bg-[#465697] text-white p-10 md:p-12 items-center'>
             <div>
                 <h1 className='text-2xl md:text-5xl font-bold'> Contact</h1>
-                <h2 className='text-sm md:text-xl font-semibold py-2'>Feel free to reach out</h2>
+                <h2 className='text-sm md:text-xl font-semibold py-2'>Feel free to reach</h2>
             </div>
             <ul className='text-sm md:text-xl'>
                 <li className='flex gap-1 items-center'>
